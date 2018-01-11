e58321045b015e449b2cb62376ad36efa4a0577a
Dima Volkov <mr.rotberry@gmail.com>
2018-01-11 03:16:18 +0200

    add test on empty list

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,22 @@
 const taskText = document.getElementById('task-text'),
   taskAddBtn = document.getElementById('task-add-btn'),
   taskError = document.getElementById('task-error'),
-  tastContainer = document.getElementById('task-container');
+  taskContainer = document.getElementById('task-container');
 
-function addTask () {
+const emptyItem = `
+  <li class="task-item task-item__empty">Empty list :( . Create your first task</li>
+`;
+
+checkEmptyList();
+
+function checkEmptyList() {
+  console.log(taskContainer.childNodes);
+  if (!taskContainer.childNodes.length) {
+    taskContainer.innerHTML = emptyItem;
+  }
+}
+
+function addTask() {
   if (taskText.value !== '') {
     taskError.classList.remove('show');
   } else {
