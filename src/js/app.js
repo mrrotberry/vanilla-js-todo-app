a2889f58d5415e7eec4c9bdaa6ca89fff55f74eb
Dima Volkov <mr.rotberry@gmail.com>
2018-01-11 15:06:40 +0200

    add possibility done task

--- src/js/app.js
+++ src/js/app.js
@@ -8,17 +8,14 @@ const emptyItem = `
 `;
 
 function renderItem(text) {
-  const item = `
-    <li class="task-item" data-id="0">
-      ${text}
-      <div class="task-item_btns">
-        <button class="task-item_btn task-item_btn__done"><span class="glyphicon glyphicon-ok"></span></button>
-        <button class="task-item_btn task-item_btn__edit"><span class="glyphicon glyphicon-pencil"></span></button>
-        <button class="task-item_btn task-item_btn__delete"><span class="glyphicon glyphicon-remove"></span></button>
-      </div>
-    </li>
+  return `
+    ${text}
+    <div class="task-item_btns">
+      <button class="task-item_btn task-item_btn__done"><span class="glyphicon glyphicon-ok"></span></button>
+      <button class="task-item_btn task-item_btn__edit"><span class="glyphicon glyphicon-pencil"></span></button>
+      <button class="task-item_btn task-item_btn__delete"><span class="glyphicon glyphicon-remove"></span></button>
+    </div>
   `;
-  return item;
 }
 
 checkEmptyList();
@@ -33,7 +30,26 @@ function addTask() {
   if (taskText.value !== '') {
     taskError.classList.remove('show');
 
-    taskContainer.innerHTML += renderItem(taskText.value);
+    const item = document.createElement('li');
+    item.classList.add('task-item');
+    item.setAttribute('data-id', '0');
+    item.innerHTML = renderItem(taskText.value);
+
+    const btnDone = item.querySelector('.task-item_btn__done');
+    btnDone.addEventListener('click', function () {
+      this.parentNode.parentNode.classList.toggle('task-item__done');
+
+      const btnDoneIcon = this.querySelector('.glyphicon');
+      if (btnDoneIcon.classList.contains('glyphicon-ok')) {
+        btnDoneIcon.classList.remove('glyphicon-ok');
+        btnDoneIcon.classList.add('glyphicon-check');
+      } else {
+        btnDoneIcon.classList.remove('glyphicon-check');
+        btnDoneIcon.classList.add('glyphicon-ok');
+      }
+    });
+
+    taskContainer.appendChild(item);
 
     taskText.value = '';
   } else {
