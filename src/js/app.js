735701ce4feec1db64d42ec456dd15967e24e41d
Dima Volkov <mr.rotberry@gmail.com>
2018-01-11 03:07:07 +0200

    add validation to add task

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('Hello World !!!')
+const taskText = document.getElementById('task-text'),
+  taskAddBtn = document.getElementById('task-add-btn'),
+  taskError = document.getElementById('task-error'),
+  tastContainer = document.getElementById('task-container');
+
+function addTask () {
+  if (taskText.value !== '') {
+    taskError.classList.remove('show');
+  } else {
+    taskError.classList.add('show');
+  }
+}
+
+taskText.addEventListener('input', () => {
+  if (taskError.classList.contains('show')) {
+    taskError.classList.remove('show');
+  }
+});
+
+taskAddBtn.addEventListener('click', addTask);
