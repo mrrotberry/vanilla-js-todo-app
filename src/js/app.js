b2f347a45c7a8dbc2f1c01b79ce141b26058f43a
Dima Volkov <mr.rotberry@gmail.com>
2018-01-11 19:47:56 +0200

    fix change text someone tasks

--- src/js/app.js
+++ src/js/app.js
@@ -61,13 +61,33 @@ function addTask() {
     btnEdit.addEventListener('click', function () {
       const thisItem = this.parentNode.parentNode;
 
-      thisItem.classList.toggle('task-item__edit');
+      if (taskContainer.querySelectorAll('.task-item__edit').length >= 1) {
+        Array.from(taskContainer.querySelectorAll('.task-item__edit')).map((itemEdit)=> {
+          if (itemEdit !== thisItem) {
+            itemEdit.querySelector('.glyphicon').classList.remove('glyphicon-edit');
+            itemEdit.querySelector('.glyphicon').classList.add('glyphicon-pencil');
+
+            const editingText = itemEdit.querySelector('.task-item-input').value;
+
+            itemEdit.querySelector('.task-item-input').remove();
+
+            itemEdit.childNodes[0].nodeValue = editingText;
+
+            itemEdit.querySelector('.task-item_btn__done').removeAttribute('hidden');
+            itemEdit.querySelector('.task-item_btn__delete').removeAttribute('hidden');
+
+            itemEdit.classList.remove('task-item__edit');
+          }
+        });
+      }
 
       const btnDone = thisItem.querySelector('.task-item_btn__done');
       const btnDelete = thisItem.querySelector('.task-item_btn__delete');
 
       const btnEditIcon = this.querySelector('.glyphicon');
       if (btnEditIcon.classList.contains('glyphicon-pencil')) {
+        thisItem.classList.add('task-item__edit');
+
         btnEditIcon.classList.remove('glyphicon-pencil');
         btnEditIcon.classList.add('glyphicon-edit');
 
@@ -95,10 +115,14 @@ function addTask() {
 
         btnDelete.setAttribute('hidden', 'hidden');
       } else {
+        thisItem.classList.remove('task-item__edit');
+
         btnEditIcon.classList.remove('glyphicon-edit');
         btnEditIcon.classList.add('glyphicon-pencil');
 
-        thisItem.querySelector('.task-item-input').remove();
+        if (thisItem.querySelector('.task-item-input')){
+          thisItem.querySelector('.task-item-input').remove();
+        }
 
         thisItem.childNodes[0].nodeValue = window.text;
 
